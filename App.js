6460d0995d944e38f4f3482518f542818d7caf7b
Your Name <you@example.com>
2024-04-08 16:19:29 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    View,
+    Text,StyleSheet, Button, Touchable, TouchableOpacity
+} from 'react-native';
+import Ex1 from './Ex1';
+
+const App = () =>
+{
+  return (
+      <Ex1/>
+  );
+} 
+export default App
